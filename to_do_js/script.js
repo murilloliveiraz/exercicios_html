9a5feb8aeaa97efc95e9a6d19e7a925e922dfb5e
murillo <murillo1047@gmail.com>
2023-02-28 15:22:24 -0300

    done

---
+++ to_do_js/script.js
@@ -0,0 +1,46 @@
+function addTask(){
+
+    const taskTitle = document.querySelector ("#task-title").value;
+
+    if(taskTitle){
+        const template = document.querySelector(".template");
+
+        const newTask = template.cloneNode(true);
+
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");  
+        
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        const doneBtn = newTask.querySelector(".done-btn").addEventListener("click" , function(){
+            completeTask(this);
+        })
+
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click" , function(){
+            removeTask(this);
+        })
+
+        document.querySelector("#task-title").value = "";
+    }
+}
+
+function removeTask(task){
+    task.parentNode.remove(true);
+}
+
+function completeTask(task){
+    const taskToComplete = task.parentNode;
+    taskToComplete.classList.toggle("done");  
+}
+
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click" , function(e){
+    e.preventDefault();
+
+    addTask();
+})
